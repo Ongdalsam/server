c4a68a6d894894f6ad9bbaf4603b28001c7156eb
Seong-Jun Yea <jukanvia1@gmail.com>
2019-09-08 16:13:29 +0900

    profile

---
+++ profile.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<title>Messy Math</title>
+		
+		 <!-- Compiled and minified CSS -->
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <nav>
+            <div class="nav-wrapper"> 
+                   <a href="https://ongdalsam.herokuapp.com/index.php" class="brand-logo" style="padding-left: 20px;">Messy Math</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+							 <li><a href="problem_list.php">Problems</a></li>
+							 <li><a href="profile.php">Profile</a></li>
+							 <li><a href="logout.php">Logout</a></li>
+                    </ul>
+              </div>
+		</nav>
+
+        <div class="container">
+            <?php echo "<h3>"Chech your profile"</h3>"; ?>
+            <br>
+            <div class="row">
+                <div class="card">
+                    <span class = "card title">당신의 이름은 :</span>
+                    <p>
+                    <?php echo "$name 입니다"; ?>
+                    </p>
+                </div>
+                <div class="card">
+                    <span class = "card title">당신의 아이디는 :</span>
+                    <p>
+                    <?php echo "$id 입니다"; ?>
+                    </p>
+                </div>
+                <div class="card">
+                    <span class = "card title">당신의 맞은 개수는 :</span>
+                    <p>
+                    <?php echo "$correct 입니다"; ?>
+                    </p>
+                </div>
+                <div class="card">
+                    <span class = "card title">당신의 정답률은 :</span>
+                    <p>
+                    <?php echo "$correct / $total 입니다"; ?>
+                    </p>
+                </div>
+            </div>
+            <br>
+        </div>
+
+        <!--JavaScript at end of body for optimized loading-->
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
