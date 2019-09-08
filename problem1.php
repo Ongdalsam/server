595e0015be1f555fcf095fd3e01477f6ee706b16
jhoonsong <edwardjhoon@gmail.com>
2019-09-08 15:20:33 +0900

    problem1-content

---
+++ problem1.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+		<title>Messy Math</title>
+		
+		 <!-- Compiled and minified CSS -->
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+        <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <nav>
+            <div class="nav-wrapper"> 
+                   <a href="https://ongdalsam.herokuapp.com/index.php" class="brand-logo" style="padding-left: 20px;">Messy Math</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+							 <li><a href="problem_list.php">Problems</a></li>
+							 <li><a href="profile.php">Profile</a></li>
+							 <li><a href="logout.php">Logout</a></li>
+                    </ul>
+              </div>
+		</nav>
+
+        <div class="container">
+            <h3>1. 계단 오르기</h3>
+            <br>
+            <div class="row">
+                <div class="card">
+                    <p>총 6개의 계단이 있다. 처음부터 시작하여 꼭대기까지 올라가는데 한 번에 한 칸, 두 칸, 또는 세 칸씩 올라갈 수 있다고 한다. 이 때 계단을 오르는 서로 다른 방법의 총 가지 수는?</p>
+                </div>
+            </div>
+            <br>
+            <form action = "check1.php" method="POST">
+                
+            </form>
+        </div>
+
+        <!--JavaScript at end of body for optimized loading-->
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
