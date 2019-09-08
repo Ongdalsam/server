7f54eefd3a10c555b2d844dd65e60beeeb185e34
jhoonsong <edwardjhoon@gmail.com>
2019-09-08 11:04:43 +0900

    a lot

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>로그인 예제</title>
+		<title>Messy Math</title>
+		
+		 <!-- Compiled and minified CSS -->
+		 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
     <body>
-            <h2>닷넷 2차 선발용 로그인 웹 템플릿<h2>
             <?php
 				session_start();
 
@@ -17,19 +29,22 @@
 				}
 				$id = $_SESSION['id'];
 				$name = $_SESSION['name'];
-
-				echo "<p>안녕하세요. $name($id)님</p>";
-
-				//
-				//contests 여기다 추가하세요
-				//
-
-				echo "<p><a href='logout.php'>로그아웃</a></p>";
 			?>
-        	<a href="signup.html" target="right">
-				<button>회원가입 하러가기</button>
-			</a>
+				<nav>
+                    <div class="nav-wrapper"> 
+                       <a href="https://ongdalsam.herokuapp.com/index.php" class="brand-logo" style="padding-left: 20px;">Messy Math</a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+								 <li><a href="problem_list.php">Problems</a></li>
+								 <li><a href="profile.php">Profile</a></li>
+								 <li><a href="logout.php">Logout</a></li>
+                        </ul>
+                  </div>
+                </nav>
 			
+				<?php echo "<p>안녕하세요. $name($id)님</p>"; ?>
+		
+		<!--JavaScript at end of body for optimized loading-->
+		<script type="text/javascript" src="js/materialize.min.js"></script>
     </body>
 
 </html> 
