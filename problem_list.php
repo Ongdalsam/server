a887a9bd6d63e6a674d5268049034ee61349aac2
jhoonsong <edwardjhoon@gmail.com>
2019-09-14 00:25:05 +0900

    debug

--- problem_list.php
+++ problem_list.php
@@ -43,16 +43,16 @@
             ?>
 
                 <?php
-                    $i = 0;
+                    //$i = 0;
                     while($row = mysqli_fetch_array($res)){
-                        $i = $i + 1; ?>
+                        //$i = $i + 1; ?>
                         <div class="row">
                             <div class="col s12">
                                 <div class="card">
                                     <span class="card-title"><?php echo "$i. .$row['name']."; ?></span>
                                     <div class="card-action">
                                         <a href="problem.php"><?php 
-                                        /*$_SESSION['prob_num'] = $i;
+                                       /* $_SESSION['prob_num'] = $i;
                                         $_SESSION['prob_name'] = $row['name'];
                                         $_SESSION['prob_content'] = $row['content'];
                                         $_SESSION['prob_answer'] = $row['answer'];*/
